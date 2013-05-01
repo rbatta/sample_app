f58fcdf0130274f288fcb9099693412b8c7c1e5e
Risa Batta <rbatta@gmail.com>
2013-05-01 07:11:21 -0700

    Finish static pages to 3.5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,26 +4,48 @@ describe "Static pages" do
 
   describe "Home page" do
 
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tut Sample App | Home")
+    end
+
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', 
+                        :text => "Sample App")
     end
+
   end
 
   describe "Help page" do
 
-  	it "should have the content 'Help'" do
-  	  visit '/static_pages/help'
-  	  page.should have_content('Help')
-  	end
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tut Sample App | Help")
+    end
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1',
+                        :text => "Help")
+    end
+
   end
 
   describe "About page" do
 
+    it "should have the title 'About Me'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tut Sample App | About Me")
+    end
+
     it "should have the content 'About Me'" do
       visit '/static_pages/about'
-      page.should have_content('About Me')
+      page.should have_selector('h1',
+                        :text => "About Me")
     end
   end
-  
 end
