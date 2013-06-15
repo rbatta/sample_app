401f4f06ca71e2a1f0e12dc9bd8449939c514a11
Risa Batta <rbatta@gmail.com>
2013-06-14 17:39:38 -0700

    Add gravatar, factory_girl gem, users page to ch 7.2

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do 
+  factory :user do
+    name		"Test test"
+    email		"test@test.com"
+    password	"testtest"
+    password_confirmation "testtest"
+  end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,10 +4,22 @@ describe "User Pages" do
   
   subject { page }
 
+  describe "profile page" do
+    # Code to make a user variable user factory_girl
+    # made it work with "no 'users' table" error by running
+    #   bundle exec rake db:test:prepare
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_selector('h1',   text: user.name ) }
+    it { should have_selector('title',  text: user.name ) }
+  end
+
   describe "sign up page" do
     before { visit signup_path }
 
     it { should have_selector('h1', text: 'Sign up') }
     it { should have_selector('title', text: full_title('Sign up')) }
   end
+
 end
