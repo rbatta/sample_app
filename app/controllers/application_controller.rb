05718cb3ecb345c84b23a9e7281179e6e62ed692
Risa Batta <rbatta@gmail.com>
2013-06-21 10:32:00 -0700

    creating a remember token for persistent signin status to ch 8.2.2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include SessionsHelper
+
+  # force signout to prevent cross-site req forgery 
+  def handle_unverified_request
+  	signout
+  	super
+  end
+ 	
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,18 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		user = User.find_by_email(params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			# if correct sign in properly and redirect to user page
+			sign_in user
+			redirect_to user
+		else
+			# if incorrect, pop up error msg and re-render signin form
+			# flash persists over other pages
+			# flash.now renders only on current page (once)
+			flash.now[:error] = 'Invalid email/password combination'
+			render 'new'
+		end
 	end
 
 	def destroy
