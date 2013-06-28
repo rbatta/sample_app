4533d4b4f15409038076236aedfc86a6cd8d8b42
Risa Batta <rbatta@gmail.com>
2013-06-28 06:29:17 -0700

    Slight typo to get rspec working properly

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -8,6 +8,6 @@ end
 
 RSpec::Matchers.define :have_error_message do |message|
 	match do |page|
-		page.should have_selector('div.alert.alert_error', text: message)
+		page.should have_selector('div.alert.alert-error', text: message)
 	end
 end
