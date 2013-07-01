d03c52fa567063a7dbe158a8a8968171725eb2b9
Risa Batta <rbatta@gmail.com>
2013-07-01 06:28:33 -0700

    yet another git recovery

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -7,7 +7,7 @@ def valid_signin(user)
 end
 
 RSpec::Matchers.define :have_error_message do |message|
-	match do |page|
-		page.should have_selector('div.alert.alert-error', text: message)
-	end
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
+  end
 end
