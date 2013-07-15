f9efb51e568ef320c256b21ff336dfcf93870b64
Risa Batta <rbatta@gmail.com>
2013-07-15 09:06:04 -0700

    create ability to delete users as admin, to 9.5

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,5 +4,9 @@ FactoryGirl.define do
     sequence(:email)	{ |n| "person_#{n}@example.com" }
     password "testtest"
     password_confirmation "testtest"
+
+    factory :admin do
+    	admin true
+    end
   end
 end
