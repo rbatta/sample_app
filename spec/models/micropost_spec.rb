93985d6ee0830ff6692048135d98ef60a265e270
Risa Batta <rbatta@gmail.com>
2013-07-15 19:27:26 -0700

    added microposts model

---
+++ spec/models/micropost_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Micropost do
+  
+  let(:user) { FactoryGirl.create(:user) }
+
+  before do
+  	@micropost = Micropost.new(content: "This is wrong", user_id: user.id)
+  end
+
+  subject { @micropost }
+
+  it { should respond_to(:content) }
+  it { should respond_to(:user_id) }
+
+  it { should be_valid }
+
+  describe "when user_id is not present" do
+
+  	before { @micropost.user_id = nil }
+
+  	it { should_not be_valid }
+  end
+end
