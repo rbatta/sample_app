71c4601ec29bd83605324733d829e1a13315c97c
Risa Batta <rbatta@gmail.com>
2013-07-15 20:44:05 -0700

    added micropost to user association, to ch 10.1.4

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -5,20 +5,29 @@ describe Micropost do
   let(:user) { FactoryGirl.create(:user) }
 
   before do
-  	@micropost = Micropost.new(content: "This is wrong", user_id: user.id)
+  #	@micropost = Micropost.new(content: "This is the wrong way", user_id: user.id)
+  	@micropost = user.microposts.build(content: "This is a post")
   end
 
   subject { @micropost }
 
   it { should respond_to(:content) }
   it { should respond_to(:user_id) }
+  it { should respond_to(:user) }
+  its(:user) { should == user }
 
   it { should be_valid }
 
   describe "when user_id is not present" do
-
   	before { @micropost.user_id = nil }
-
   	it { should_not be_valid }
+  end
+
+  describe "accessible attributes" do
+  	it "should not allow access to user_id" do
+  		expect do
+  			Micropost.new(user_id: user.id)
+  		end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+  	end
   end
 end
